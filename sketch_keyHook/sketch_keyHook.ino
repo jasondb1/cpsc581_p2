13e7d2ea6ccbedbc3ee1d91b37fc9d70ab429406
cynthiaxw <wangxi.cynthia1@gmail.com>
2019-03-13 15:59:45 -0600

    Added simple logic that works for one key.

--- sketch_keyHook/sketch_keyHook.ino
+++ sketch_keyHook/sketch_keyHook.ino
@@ -1,48 +1,43 @@
-// Key
-//
-//
-//
-//
-//
-//
-//
-//
-
-
-
 
 //pin setup
 //output pins
 #define LED_R1 30
 #define LED_G1 31
-#define LED_R2 34
-#define LED_G2 35
-#define LED_R3 38
-#define LED_G3 39
+#define LED_R2 32
+#define LED_G2 33
+#define LED_R3 34
+#define LED_G3 35
 #define BUZZER 22
 #define LED_BOARD = LED_BUILTIN //This could be used for error signalling
 
 const int LED[] = {LED_R1, LED_G1, LED_R2, LED_G2, LED_R3, LED_G3};
 
 //input pins
-#define KEY_1 46
-#define KEY_2 47
-#define KEY_3 48
-#define SW_1 50
-#define SW_2 51
-#define SW_3 52
+#define KEY_1 51
+#define KEY_2 52
+#define KEY_3 53
 
-#define PIR_1 42
-#define PIR_2 43
+#define PIR_1 24
+#define BTN_1 40
 
-const int IN[] = {KEY_1, KEY_2, KEY_3, SW_1, SW_2, SW_3, PIR_1, PIR_2};
+
+
+const int IN[] = {KEY_1, KEY_2, KEY_3, PIR_1, BTN_1};
 const int KEY[] = {KEY_1, KEY_2, KEY_3};
+
+enum state{ALARM_OFF, ALARM_ON, CLOSED, OPEN};
+
+int sensorPIR = 0;
+state doorState = CLOSED;
+state systemState = ALARM_OFF;
+int preKeyNum = 0;
+int keyNum = 0;
+boolean keyChangeFlg = false;
 
 ////////////////////////////////////////////
 // setup
 //
 //
-
 void setup() {
 
   Serial.begin(9600);
@@ -61,26 +56,111 @@ void setup() {
   pinMode(KEY_1, INPUT);
   pinMode(KEY_2, INPUT);
   pinMode(KEY_3, INPUT);
-  pinMode(SW_1, INPUT);
-  pinMode(SW_2, INPUT);
-  pinMode(SW_3, INPUT);
   pinMode(PIR_1, INPUT);
-  pinMode(PIR_2, INPUT);
+  pinMode(BTN_1, INPUT);
 
-  //Runs a dignostic test to demonstrate leds / and buzzer working
-  selfTest();
-  
+  digitalWrite(KEY_1, HIGH);
+  digitalWrite(KEY_2, HIGH);
+  digitalWrite(KEY_3, HIGH);
 
+  int keyCheck = 0;
+  preKeyNum = currentKeyNum();
+  while(keyCheck < 3){  // Eliminate noise
+    keyNum = currentKeyNum();
+    if(keyNum == preKeyNum)keyCheck ++;
+    else{
+      preKeyNum = keyNum;
+      keyCheck = 0;
+    }
+    delay(50);
+  }
 }
-
-
-
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(ALARM_OFF == systemState){
+    if(doorOpen()){
+      if(!keyChangeFlg){
+        systemState = ALARM_ON;
+      }else{
+        
+      }
+    }else{
+      doorState = CLOSED;
+    }
+  }else if(ALARM_ON == systemState){
+    if(keyChangeFlg){
+      turnOffAlarm();
+      delay(5000); //deley 5 sec to reset the system
+      // reset system
+      preKeyNum = keyNum;
+      keyChangeFlg = false;
+      systemState = ALARM_OFF;
+      doorState = CLOSED;
+    }else{
+      activateAlarm();
+    }
+  }
+  keyChangeFlg = keyChanged();  
+  delay(50);
 }
 
+
+///////////////////////////////////
+// Detect door activity
+//
+boolean doorOpen() {
+  sensorPIR = digitalRead(PIR_1);
+  if(HIGH == sensorPIR){  // motion detected
+    if(CLOSED == doorState){
+      doorState = OPEN;
+      return true;
+    }
+  }
+  return false;
+}
+
+///////////////////////////////////
+// Detect if the key number has changed
+//
+boolean keyChanged() {
+  keyNum = currentKeyNum();
+  if(keyNum == preKeyNum)return false;
+  delay(50);  // eliminate noise
+  if(currentKeyNum() == keyNum){
+    preKeyNum = keyNum;
+    return true;
+  }
+  return false;
+}
+
+///////////////////////////////////
+// Check current number of keys
+//
+int currentKeyNum() {
+  int cnt=0; 
+  for(int i=0; i<(sizeof(KEY) / sizeof(KEY[0])); i++){
+    if(!digitalRead(KEY[i]))cnt ++;
+  }
+  return cnt;
+}
+
+void turnOffAlarm(){
+  buzzerOff();
+  for (int i = 0; i < (sizeof(LED) / sizeof(LED[0])); i++) {
+    ledOff(LED[i]);
+  }
+}
+
+void activateAlarm(){
+  buzzerOn();
+  for (int i = 0; i < (sizeof(LED) / sizeof(LED[0])); i++) {
+    ledOn(LED[i]);
+    delay(50);
+    ledOff(LED[i]);
+    delay(50);
+  }
+}
 
 void selfTest() {
 
